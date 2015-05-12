2351880de31e9e7e37e68fd145ced4c10f0136c0
Yves Van Broekhoven <yvesvanbroekhoven@gmail.com>
2015-05-12 11:43:14 +0200

    Documented utils module

--- gulp/assets/javascripts/modules/utils.js
+++ gulp/assets/javascripts/modules/utils.js
@@ -1,5 +1,26 @@
 let utils = {
 
+  /*
+   * Strip out script tags of jQuery ajax response data
+   *
+   * @return {jQuery object}
+   */
+  cleanResponse: function(respData) {
+    var regex = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
+    return $('<div>').append( respData.replace(regex, '') );
+  },
+
+  /* Returns a function, that, as long as it continues to be invoked, will not
+   * be triggered. The function will be called after it stops being called for
+   * N milliseconds. If `immediate` is passed, trigger the function on the
+   * leading edge, instead of the trailing.
+   *
+   * @param {Function} func
+   * @param {Integer}  wait
+   * @param {Boolean}  immediate
+   *
+   * @return {Function}
+   */
   debounce: function(func, wait, immediate) {
     var timeout;
 
@@ -16,6 +37,16 @@ let utils = {
     };
   },
 
+  /**
+   * Fire a function with pauses
+   *
+   * @param  {Integer}            delay
+   * @param  {Boolean|Function}   no_trailing
+   * @param  {Function}           callback
+   * @param  {Boolean}            debounce_mode
+   *
+   * @return {Function}
+   */
   throttle: function(delay, no_trailing, callback, debounce_mode) {
     var timeout_id,
         last_exec = 0;
