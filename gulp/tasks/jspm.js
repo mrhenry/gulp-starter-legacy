cc11e65df28c773bfaf618ce8ad4fa38c8253452
Yves Van Broekhoven <yvesvanbroekhoven@gmail.com>
2015-08-04 13:12:51 +0200

    Fixed gulp jspm wrong src

--- gulp/tasks/jspm.js
+++ gulp/tasks/jspm.js
@@ -5,12 +5,12 @@ var jspm = require('jspm');
 gulp.task('jspm', function() {
 
   gulp.src(['public/assets/jspm_packages/*.js', 'public/assets/jspm_packages/*.js.map'])
-      .pipe(changed('assets/jspm_packages/')) // Ignore unchanged files
-      .pipe(gulp.dest('assets/jspm_packages/'))
+      .pipe(changed('public/assets/jspm_packages/')) // Ignore unchanged files
+      .pipe(gulp.dest('public/assets/jspm_packages/'))
 
-  gulp.src(['app/config.js'])
-      .pipe(changed('assets/'))
-      .pipe(gulp.dest('assets/'));
+  gulp.src(['public/assets/config.js'])
+      .pipe(changed('public/assets/'))
+      .pipe(gulp.dest('public/assets/'));
 
   return jspm.bundle('application', 'public/assets/bundle.js',
     {
